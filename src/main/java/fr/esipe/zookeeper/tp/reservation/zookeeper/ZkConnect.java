dbba8140ab65e461eb5592a45149b3d3736f2a27
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2018-02-13 22:08:19 +0100

    getZNodeData method added

--- src/main/java/fr/esipe/zookeeper/tp/reservation/zookeeper/ZkConnect.java
+++ src/main/java/fr/esipe/zookeeper/tp/reservation/zookeeper/ZkConnect.java
@@ -3,22 +3,22 @@ package fr.esipe.zookeeper.tp.reservation.zookeeper;
 /**
  * Created by Vyach on 13/02/2018.
  */
-import java.util.Date;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.CountDownLatch;
 
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.Watcher.Event.KeeperState;
 import org.apache.zookeeper.ZooDefs.Ids;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ZkConnect {
     private ZooKeeper zk;
     private CountDownLatch connSignal = new CountDownLatch(0);
 
-    //host should be 127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002
+    private static final Logger logger = LoggerFactory.getLogger(ZkConnect.class);
+
     public ZooKeeper connect(String host) throws Exception {
         zk = new ZooKeeper(host, 3000, new Watcher() {
             public void process(WatchedEvent event) {
@@ -45,6 +45,31 @@ public class ZkConnect {
 
     public void deleteNode(String path) throws Exception {
         zk.delete(path, zk.exists(path, true).getVersion());
+    }
+
+
+    public Stat getZNodeStats(String path) throws KeeperException,
+            InterruptedException {
+        Stat stat = zk.exists(path, true);
+        if (stat != null) {
+            logger.info("Node exists and the node version is "+ stat.getVersion());
+        } else {
+            logger.info("Node does not exists");
+        }
+        return stat;
+    }
+
+    public Object getZNodeData(String path) throws KeeperException, InterruptedException, UnsupportedEncodingException {
+
+        Stat stat = getZNodeStats(path);
+        byte[] b = null;
+        if (stat != null) {
+            b = zk.getData(path, true, null);
+        }
+
+        String data = new String(b, "UTF-8");
+
+        return data;
     }
 
 }
