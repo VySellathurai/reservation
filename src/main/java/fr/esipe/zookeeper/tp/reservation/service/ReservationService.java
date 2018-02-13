9cd346ad3c5c379f7fac71ee19409321d67da0b6
vyach.sellathurai <vyach.sellathurai@etu.u-pec.fr>
2018-02-13 20:45:44 +0100

    project correction

--- src/main/java/fr/esipe/zookeeper/tp/reservation/service/ReservationService.java
+++ src/main/java/fr/esipe/zookeeper/tp/reservation/service/ReservationService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HelloWorldService {
+public class ReservationService {
 
     private String name = "World";
 
--- src/main/java/fr/esipe/zookeeper/tp/reservation/web/AppController.java
+++ src/main/java/fr/esipe/zookeeper/tp/reservation/web/AppController.java
@@ -3,24 +3,20 @@ package fr.esipe.zookeeper.tp.reservation.web;
 /**
  * Created by Vyach on 13/02/2018.
  */
-import fr.esipe.zookeeper.tp.reservation.service.HelloWorldService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AppController {
 
 
-    @RequestMapping("/")
-    public String index(Model model) {
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
 
-        model.addAttribute("message", "World");
 
-        return "index";
+        return "Application is running !";
     }
 
 }
